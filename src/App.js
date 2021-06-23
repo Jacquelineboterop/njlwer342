0ac256074074c441b90fa1e5989387995954fe3b
Jacqueline <proyectos@branta.com.co>
2021-06-22 19:45:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={html : [], text : ""}
+   }
+   handleSubmit = (event) => {
+  //console.log(event)
+  event.preventDefault()
+  let html = this.state.html
+  html.push(this.state.text)
+  //console.log(html)
+
+  this.setState({html,text:""})
+   }
+   handleChange = (event) => {
+   this.setState({text: event.target.value})
+     }
   render() {
     return (
       <div className="wrapper">
@@ -12,9 +28,10 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.html.map(( element) => <li>{element}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.handleSubmit}>
+             <input type="text"  id="new-task" value={this.state.text} onChange={this.handleChange} placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
